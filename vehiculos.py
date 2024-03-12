3cf5d05fe2fe5fa687683acf752b835a968b1b76
AmaroMOO <amaroromerocedrez@gmail.com>
2024-03-12 11:44:44 +0000

    vehiculos planteado

---
+++ vehiculos.py
@@ -0,0 +1,9 @@
+class Vehiculo:
+    def __init__(self) -> None:
+        self.ruedas = ruedas
+        self.color = color
+        ruedas = 0
+        color = ""
+
+    def info(self):
+        print(f"El vehiculo tiene {self.ruedas} ruedas con un color {self.color}")
