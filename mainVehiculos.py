61065979436eb58d607d3835a923d4d0295f5752
AmaroMOO <amaroromerocedrez@gmail.com>
2024-03-12 12:24:39 +0000

    vehiculo y main vehiculo modificados

--- mainVehiculos.py
+++ mainVehiculos.py
@@ -1,10 +1,14 @@
 from automovil import automovil
 
 def main():
+    automovil = automovil(4, "amarillo")
     camion = automovil(8, "azul")
     moto = automovil(3, "rojo")
+    bicicleta = automovil(2, "negra")
 
     camion.info()
-    motos.info()
+    moto.info()
+    automovil.info()
+    bicicleta.info()
 
 main()
--- vehiculos.py
+++ vehiculos.py
@@ -1,9 +1,9 @@
 class Vehiculo:
-    def __init__(self) -> None:
+    def __init__(self, ruedas:int, color:str) -> None:
         self.ruedas = ruedas
         self.color = color
         ruedas = 0
         color = ""
 
-    def info(self):
-        print(f"El vehiculo tiene {self.ruedas} ruedas con un color {self.color}")
+    def info(self)->None:
+        print(self.ruedas, self.color)
